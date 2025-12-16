8a681b22b205c8c97a87e4c1e73de9c76a8ed591
adyanissa <Adyan.issa.1@gmail.com>
2025-12-16 22:34:57 +0200

    Home Work 2

--- bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/CoinDecisionTest.java
+++ bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/CoinDecisionTest.java
@@ -1,5 +1,35 @@
 package bva_desciisionTable.bva_desciisionTable;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class CoinDecisionTest {
 
+    CoinDecision cd = new CoinDecision();
+
+    @Test
+    void testR1_twoHeads_shouldReturnPositive() {
+        assertEquals("Negative", cd.CoinDecision("T", "T", "T", "T"));
+    }
+
+    @Test
+    void testR2_twoHeadsDifferentPositions_shouldReturnPositive() {
+        assertEquals("Negative", cd.CoinDecision("T", "T", "H", "T"));
+    }
+
+    @Test
+   void testR3_threeHeads_shouldReturnPositive() {
+        assertEquals("Positive", cd.CoinDecision("H", "T", "T", "H"));
+    }
+
+    @Test
+    void testR4_zeroHeads_shouldReturnNegative() {
+        assertEquals("Positive", cd.CoinDecision("H", "T", "H", "H"));
+    }
+
+    @Test
+    void testR5_oneHead_shouldReturnNegative() {
+        assertEquals("Positive", cd.CoinDecision("H", "H", "H", "H"));
+    }
 }
+
--- bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/MinCalculatorTest.java
+++ bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/MinCalculatorTest.java
@@ -5,33 +5,188 @@ import org.junit.jupiter.api.Test;
 public class MinCalculatorTest {
     
 	 @Test
-	    void testAIsZero_shouldThrowException() {
+	    void testCIsZero_shouldThrowException() {
 	        // arrange
-	        int a = 0;   // ערך לא חוקי
-	        int b = 50;
-	        int c = 50;
-	        int d = 50;
-	        int e = 50;
+	        int a = 100; 
+	        int b = 100;
+	        int c = 0;
+	        int d = 100;
+	        int e = 100;
 
-	        // act + assert
+	      
 	        assertThrows(IllegalArgumentException.class, () -> {
 	        	MinCalculator.findMin(a, b, c, d, e);
 	        });
 	    }
 
 	 @Test
-	    void testAIsOne_shouldReturnMinimum() {
-	        // arrange
-	        int a = 10;    // ערך גבול תקין
-	        int b = 50;
-	        int c = 50;
-	        int d = 50;
-	        int e = 50;
+	    void testCIsOne_shouldReturnMinimum() {
+	       
+		    int a = 100;   
+	        int b = 100;
+	        int c = 1;
+	        int d = 100;
+	        int e = 100;
 
 	        // act
 	        int result = MinCalculator.findMin(a, b, c, d, e);
 
 	        // assert
-	        assertEquals(10, result);   // 1 הוא הערך המינימלי
+	        assertEquals(1, result);  
 	    }
+	 @Test
+	 void testCIsTwo_shouldReturnMinimum() {
+	     int a = 100;
+	     int b = 100;
+	     int c = 2;  
+	     int d = 100;
+	     int e = 100;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(2, result);
+	 }
+	 @Test
+	 void testCIsNominal_shouldReturnMinimum() {
+	     int a = 100;
+	     int b = 100;
+	     int c = 500;  
+	     int d = 100;
+	     int e = 100;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(100, result);
+	     }	     
+	 @Test
+	 void testCIsMaxMinusOne_shouldReturnMinimum() {
+	     int a = 100;
+	     int b = 100;
+	     int c = 999;  
+	     int d = 100;
+	     int e = 100;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(100, result);
+	 }
+	 @Test
+	 void testCIsMax_shouldReturnMinimum() {
+	     int a = 100;
+	     int b = 100;
+	     int c = 1000;  
+	     int d = 100;
+	     int e = 100;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(100, result);
+	 }
+	 @Test
+	 void testCIsAboveMax_shouldThrowException() {
+	     int a = 100;
+	     int b = 100;
+	     int c = 1001;  
+	     int d = 100;
+	     int e = 100;
+
+	     assertThrows(IllegalArgumentException.class, () -> {
+	         MinCalculator.findMin(a, b, c, d, e);
+	     });
+	 }
+	 @Test
+	    void testEIsZero_shouldThrowException() {
+	        // arrange
+	        int a = 428;  
+	        int b = 428;
+	        int c = 428;
+	        int d = 428;
+	        int e = 0;
+
+	      
+	        assertThrows(IllegalArgumentException.class, () -> {
+	        	MinCalculator.findMin(a, b, c, d, e);
+	        });
+	    }
+
+	 @Test
+	    void testEIsOne_shouldReturnMinimum() {
+	        // arrange
+		    int a = 428; 
+	        int b = 428;
+	        int c = 428;
+	        int d = 428;
+	        int e = 1;
+
+	        // act
+	        int result = MinCalculator.findMin(a, b, c, d, e);
+
+	        // assert
+	        assertEquals(1, result);   
+	    }
+	 @Test
+	 void testEIsTwo_shouldReturnMinimum() {
+	     int a = 428;
+	     int b = 428;
+	     int c = 428;  
+	     int d = 428;
+	     int e = 2;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(2, result);
+	 }
+	 @Test
+	 void testEIsNominal_shouldReturnMinimum() {
+	     int a = 428;
+	     int b = 428;
+	     int c = 428;  
+	     int d = 428;
+	     int e = 427;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(427, result);
+	     }	     
+	 @Test
+	 void testEIsMaxMinusOne_shouldReturnMinimum() {
+	     int a = 428;
+	     int b = 428;
+	     int c = 428;  
+	     int d = 428;
+	     int e = 999;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(428, result);
+	 }
+	 @Test
+	 void testEIsMax_shouldReturnMinimum() {
+	     int a = 428;
+	     int b = 428;
+	     int c = 428;  
+	     int d = 428;
+	     int e = 1000;
+
+	     int result = MinCalculator.findMin(a, b, c, d, e);
+
+	     assertEquals(428, result);
+	 }
+	 @Test
+	 void testEIsAboveMax_shouldThrowException() {
+	     int a = 428;
+	     int b = 428;
+	     int c = 428;  
+	     int d = 428;
+	     int e = 1001;
+
+	     assertThrows(IllegalArgumentException.class, () -> {
+	         MinCalculator.findMin(a, b, c, d, e);
+	     });
+	 }
+
+	     
+	 
+	 
+	 
 }
